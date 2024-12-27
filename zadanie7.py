3f31532af1b5a822575888354754560a0db72d5b
ZuzannaKochowska <zuzannakochowska@gmail.com>
2024-12-27 15:37:33 +0100

    Task7 remark

--- zadanie7.py
+++ zadanie7.py
@@ -6,22 +6,30 @@ def catalan_numbers_below_milion():
     catalans = []
     even_count = 0
     odd_count = 0
+    even_count_list = []
+    odd_count_list = []
 
     while C < 10**6:
+        C = (4 * n + 2) * C // (n + 2)
+        n += 1
         catalans.append(C)
+
         if C % 2 == 0:
             even_count +=1
+            even_count_list.append(C)
         else: 
             odd_count +=1
+            odd_count_list.append(C)
     
-    C = (4 * n + 2) * C // (n + 2)
-    n += 1
-
+    
     print("Liczby katalońskie to: ")
-    print(catalans)
+    print(catalans, end=",")
+    print( )
     print("Parzystych liczb jest: ")
     print(even_count)
+    print("Są to liczby: ", even_count_list)
     print("Nieparzystych liczb jest: ")
     print(odd_count)
+    print("Są to liczby: ", odd_count_list)
 
 catalan_numbers_below_milion()
---
+++ zadanie8.py
@@ -0,0 +1,11 @@
+def natural_numbers():
+    natural = []
+    N = 0
+
+    while N < 20:
+        natural.append(N)
+        N =+ 1
+
+    print(natural)
+
+natural_numbers()
